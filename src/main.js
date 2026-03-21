9e82a692bb7061a72823cba8c61fd4042f9464b3
David Geiger <subscriptions.d@protonmail.com>
2026-03-21 22:24:03 +0100

    Added toggle button for light and dark mode

--- src/main.js
+++ src/main.js
@@ -8,8 +8,24 @@ saveButtonEl.addEventListener("click", saveJoke);
 
 const jokeCurrentEl = document.querySelector(".joke__current-joke");
 
+const toggleDarkModeBtn = document.querySelector(".toggle-dark-mode");
+toggleDarkModeBtn.addEventListener("click", toggleMode);
+
 async function main() {
   let joke = await getJoke();
   jokeCurrentEl.textContent = joke.text;
   saveButtonEl.style.display = "flex";
+}
+
+function toggleMode() {
+  document.querySelector(".app").classList.toggle("app-light");
+  document.querySelector(".joke").classList.toggle("joke-light");
+  document.querySelector(".joke__btnSave").classList.toggle("joke-light");
+  const savedJokes = document.querySelectorAll(".saved-joke");
+  savedJokes.forEach((joke) => {
+    joke.classList.toggle("saved-joke-light");
+  });
+  document
+    .querySelector(".toggle-dark-mode")
+    .classList.toggle("dark-background");
 }
